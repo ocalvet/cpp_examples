a46c237e6a31b701de4e9d0e9eb36d6ef0deb0bd
Ovidio R. Calvet <calveto@gmail.com>
2019-02-17 09:59:16 -0500

    Better project structure

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     Calculator *calculator = new Calculator();
-    calculator->addNumber(1);
+    calculator->addNumber(12);
     calculator->addNumber(10);
     calculator->addNumber(1);
     calculator->calculate(new SumOperation());
